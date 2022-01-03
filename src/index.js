4869e46083da6a09cdd9a266274fed4505422ef4
Sean Martin <sean@seanmartinstudio.com>
2022-01-03 11:46:52 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 console.log('%c HI', 'color: firebrick')
+
+//URL 1
+//This function fetches the images from the URL, converts JSON data to a variable and iterates...
+//over each image with a forEach loop.
+function getDataChallenge1() {
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const promise1 = fetch(imgUrl)
+.then(response => response.json())
+.then(data => {
+    const dogPics = data
+    dogPics.message.forEach(element => console.log(element))
+
+    
+})
+}
+getDataChallenge1()
+
+//This function should build out the DOM elements
+function buildHTMLChallenge1() {
+console.log(element)
+}
