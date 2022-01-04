d8ebbdfe92a75a3e4017a7a4a4296d7c4569c5ac
Sean Martin <sean@seanmartinstudio.com>
2022-01-03 16:04:09 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 console.log('%c HI', 'color: firebrick')
-const dogImgContainer = document.getElementById("dog-image-container")
-console.log(dogImgContainer)
+// const dogImgContainer = document.getElementById("dog-image-container")
+// console.log(dogImgContainer)
 
-//URL 1
+//Challenge #1
 //This function fetches the images from the URL, converts JSON data to a variable and iterates...
 //over each image with a forEach loop.
 function getDataChallenge1() {
@@ -20,8 +20,28 @@ getDataChallenge1()
 
 //This function should build out the DOM elements
 function renderImage(picURL) {
+    const dogImgContainer = document.getElementById("dog-image-container")
     const imgTag = document.createElement('img')
     imgTag.src = picURL
-    console.log(imgTag)
     dogImgContainer.appendChild(imgTag)
+}
+
+//Challenge #2
+function getDataChallenge2() {
+fetch('https://dog.ceo/api/breeds/list/all')
+.then(response => response.json())
+.then(data => {
+    const dogData = data
+    const dogDataObject = Object.entries(dogData.message)
+    dogDataObject.forEach(breeds => rednerBreeds(breeds))
+    
+})
+}
+getDataChallenge2()
+
+function rednerBreeds(breeds) {
+    const breedNameContainer = document.getElementById('dog-breeds')
+    const li = document.createElement('li')
+    li.src = breeds
+    breedNameContainer.appendChild(li)
 }
